4584d23692d368430e6ab60bb6181b80c916eed6
Cuongnguyen20001605 <69945967+Cuongnguyen20001605@users.noreply.github.com>
2021-02-05 19:50:28 +0700

    update code

--- middlewares/verifyToken.js
+++ middlewares/verifyToken.js
@@ -3,7 +3,7 @@ require("dotenv").config()
 
 
 function auth(req,res,next){
-     const token=req.header('auth-token')
+     const token=req.header('token')
      if(!token) return res.status(401).send("yeu cau xac thuc nguoi dung")
      try{
           const verifyed=jwt.verify(token,process.env.TOKEN)
